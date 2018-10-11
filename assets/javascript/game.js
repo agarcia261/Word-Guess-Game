4dba65a5863c6c2cac424603481785938458b53c
Angel <agarcia261@gmail.com>
2018-10-11 17:14:51 -0400

    more changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -20,8 +20,30 @@ $(document).ready(function() {
         },
         
     }
+
     var wordpick=Math.floor(Math.random()*words.wordsarray.length);
-    console.log(words.wordsarray[wordpick]);
+    var wordchosen = words.wordsarray[wordpick];
+
+    for (i=0; i<wordchosen.length; i++){
+        //create a new element div to have the char inside and assign a class of hideletter
+        var innerCharDivs= document.createElement("div");
+        innerCharDivs.setAttribute("class", "hideletter innerposition"+ wordchosen.charAt(i));
+        innerCharDivs.innerHTML=wordchosen.charAt(i);
+        
+        /*create a new element div to have the previoius created div inside 
+        so i can hide the letter while keeping a bottom border visible*/
+
+        outerCharDivs=document.createElement("div");
+        outerCharDivs.setAttribute("class", "position-letter");
+        $(outerCharDivs).append(innerCharDivs);
+        //I will append this to the ID Created on the HTML
+        $("#word-to-guess").append(outerCharDivs);
+    }
+
+    document.onkeyup = function(event){        
+        console.log(event)
+};
+    
 
 
 
