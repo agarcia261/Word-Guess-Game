878fce4f38da279fc7facef3b8ad76bc12f63628
Angel <agarcia261@gmail.com>
2018-10-12 17:58:16 -0400

    Program now works for a single game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
 
-
-
     var words= {
-        wordsarray:["test1","longerword","test3"],
+        wordsarray:["test 1","longerword","hupa lupa"],
+        charArray:[],
         wins:0,
+        loss:0,
         numberofGuess:9,
         letterguessed:[],
         matchingPos:[],
+        correctguess:[],
         notAmatch: function(){
                 this.numberofGuess--;
                 if(this.numberofGuess==0){
@@ -17,7 +18,21 @@ $(document).ready(function() {
         },
         userwon: function() {
             this.wins++
-            this.letterguessed.splice()
+            this.charArray=[];
+            this.numberofGuess=9,
+            this.letterguessed=[];
+            this.matchingPos=[];
+            this.correctguess=[];
+            audioElement.play();
+
+        },
+        userlost: function (){
+            this.loss++
+            this.charArray=[];
+            this.numberofGuess=9,
+            this.letterguessed=[];
+            this.matchingPos=[];
+            this.correctguess=[];
         },
         pushkey:function(key){
             if(this.letterguessed.indexOf(key)==-1){
@@ -29,11 +44,28 @@ $(document).ready(function() {
             }
 
         },
+        pushWordChar:function(key){
+            if(this.charArray.indexOf(key)==-1){
+                this.charArray.push(key);     
+            }
+            return 
+        },
+        alertWin: function(){
+            alert(" You Wont. Wo hoo!")        
+        },
         searchkey: function(key){
             var isKeyPressedAlready=this.pushkey(key);
             if (this.numberofGuess==0){
-                var restartgame=prompt("want to restart the game?")
-                console.log(restartgame )
+                var restartgame=confirm("want to restart the game?")
+                if (restartgame){
+
+                    alert ("i need to restart")
+                }
+                else{
+
+                    alert ("Thanks for Playing. See you soon!")
+
+                }
             }
             else if (!isKeyPressedAlready){
                 var hits=false;
@@ -41,6 +73,7 @@ $(document).ready(function() {
                     if(wordchosen.charAt(i)==key) {
                         this.matchingPos.push(i);
                         hits=true;
+                        $("."+i).removeClass("hideletter");
                     }
 
                 }
@@ -49,11 +82,17 @@ $(document).ready(function() {
                 }
                 else if(!hits && this.numberofGuess==0){
                     alert("Game over Bitch")
+                }
+                else if (hits){
+                   this.correctguess.push(key)
+                    if (this.charArray.length==this.correctguess.length){
+                    this.userwon()
+                    window.setTimeout(this.alertWin, 300);
+                    }
+                }
+                else if (hits){
 
                 }
-                console.log(this.letterguessed)
-
-                console.log(this.numberofGuess)
             }
             else {
                 alert ("This key has already been pressed. Please try another one")
@@ -65,11 +104,14 @@ $(document).ready(function() {
 
     var wordpick=Math.floor(Math.random()*words.wordsarray.length);
     var wordchosen = words.wordsarray[wordpick];
+    var audioElement = document.createElement("audio");
+    audioElement.setAttribute("src", "assets/images/champions.mp3");
+
 
     for (i=0; i<wordchosen.length; i++){
         //create a new element div to have the char inside and assign a class of hideletter
         var innerCharDivs= document.createElement("div");
-        innerCharDivs.setAttribute("class", "hideletter innerposition"+ wordchosen.charAt(i));
+        innerCharDivs.setAttribute("class", i+ " hideletter innerposition"+ wordchosen.charAt(i));
         innerCharDivs.innerHTML=wordchosen.charAt(i);
         
         /*create a new element div to have the previoius created div inside 
@@ -80,9 +122,14 @@ $(document).ready(function() {
         $(outerCharDivs).append(innerCharDivs);
         //I will append this to the ID Created on the HTML
         $("#word-to-guess").append(outerCharDivs);
+
+        words.pushWordChar(wordchosen.charAt(i))
+        console.log(words.charArray)
     }
     document.onkeyup = function(event){  
-        var keyhit=words.searchkey(event.key)      
+        var keyhit=words.searchkey(event.key)  
+        
+
 };
     
 
