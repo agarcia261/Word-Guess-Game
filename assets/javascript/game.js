d39f2c3bda250726d8a8db9147eaf3fffe41c9e9
Angel <agarcia261@gmail.com>
2018-10-12 22:20:31 -0400

    Final Version with some css and some images and sound

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
 
     var words= {
-        wordsarray:["test 1","longerword","hupa lupa"],
+        wordsarray:[item1={name:"minion", hint:"<img  class='image tobedeleted' src='assets/images/minion.png' alt='Minion'>"},
+        item2={name:"banana", hint:"<img  class='image tobedeleted' src='assets/images/banana.png' alt='Minion Bananas'>"},
+        item3={name:"pirate", hint:"<img  class='image tobedeleted' src='assets/images/pirate.png' alt='Minion pirate'>"}],
+        wordchosen:"",
         charArray:[],
         wins:0,
         loss:0,
@@ -9,10 +12,56 @@ $(document).ready(function() {
         letterguessed:[],
         matchingPos:[],
         correctguess:[],
+        selectword: function(){
+            this.updatehtml("n-guess");
+            this.updatehtml("wins");
+            this.updatehtml("letters-guessed");
+            var wordpick=Math.floor(Math.random()*this.wordsarray.length);
+             this.wordchosen = this.wordsarray[wordpick].name;
+             for (i=0; i<this.wordchosen.length; i++){
+                //create a new element div to have the char inside and assign a class of hideletter
+                var innerCharDivs= document.createElement("div");
+                innerCharDivs.setAttribute("class", i+ " hideletter innerposition");
+                innerCharDivs.innerHTML=this.wordchosen.charAt(i);
+                
+                /*create a new element div to have the previoius created div inside 
+                so i can hide the letter while keeping a bottom border visible*/
+        
+                outerCharDivs=document.createElement("div");
+                outerCharDivs.setAttribute("class", "position-letter tobedeleted");
+                $(outerCharDivs).append(innerCharDivs);
+                //I will append this to the ID Created on the HTML
+                $("#word-to-guess").append(outerCharDivs);
+                this.pushWordChar(this.wordchosen.charAt(i))
+            }
+            var image=$(".image-section")
+            image.append(this.wordsarray[wordpick].hint)
+            image.animate({width:"50px"});
+            image.animate({width:"400px"});
+
+
+        },
+        cleardiv: function() {
+            $(".tobedeleted").remove()
+        },
+        updatehtml: function (idtoupdate){
+            var gRemaining=document.getElementById(idtoupdate);
+            if (idtoupdate=="n-guess"){
+                gRemaining.innerHTML=this.numberofGuess;
+            }
+            else if (idtoupdate=="wins"){
+                gRemaining.innerHTML=this.wins;
+            }
+            else if (idtoupdate=="letters-guessed"){
+                gRemaining.innerHTML=this.letterguessed
+            }
+        },
         notAmatch: function(){
                 this.numberofGuess--;
+                this.updatehtml("n-guess")
                 if(this.numberofGuess==0){
-                   alert("Game over Bitch")
+                   alert("You Lost")
+                   this.userlost();
                 }
             
         },
@@ -23,24 +72,55 @@ $(document).ready(function() {
             this.letterguessed=[];
             this.matchingPos=[];
             this.correctguess=[];
+            this.cleardiv();
+            this.updatehtml("n-guess");
+            this.updatehtml("wins");
+            this.selectword();
             audioElement.play();
+            this.musicstop();
+
+        },
+        musicstop: function (){
+            window.setTimeout(stopmusic, 30000);
+            function stopmusic (){
+            audioElement.pause();
+            }
 
         },
         userlost: function (){
+            this.cleardiv();
+            var image=$(".image-section")
+            image.append("<img  class='image tobedeleted' src='assets/images/hangman_game.png' alt='Minion pirate'>")
+            image.animate({width:"50px"});
+            image.animate({width:"400px"});
             this.loss++
+        },
+        restartgameafterloss: function(){
             this.charArray=[];
             this.numberofGuess=9,
             this.letterguessed=[];
             this.matchingPos=[];
             this.correctguess=[];
+            this.cleardiv();
+            this.updatehtml("n-guess");
+            this.updatehtml("wins");
+            this.selectword();
+            
         },
         pushkey:function(key){
-            if(this.letterguessed.indexOf(key)==-1){
-                this.letterguessed.push(key);
-                return false
+            if (this.numberofGuess!=0){
+                if(this.letterguessed.indexOf(key)==-1){
+                    this.letterguessed.push(key);
+                    this.updatehtml("letters-guessed");
+
+                    return false
+                }
+                else{
+                    return true
+                }
             }
             else{
-                return true
+                return
             }
 
         },
@@ -56,10 +136,10 @@ $(document).ready(function() {
         searchkey: function(key){
             var isKeyPressedAlready=this.pushkey(key);
             if (this.numberofGuess==0){
-                var restartgame=confirm("want to restart the game?")
+                var restartgame=confirm("want to keep playing?")
                 if (restartgame){
 
-                    alert ("i need to restart")
+                    this.restartgameafterloss()
                 }
                 else{
 
@@ -69,8 +149,8 @@ $(document).ready(function() {
             }
             else if (!isKeyPressedAlready){
                 var hits=false;
-                for (i=0; i<this.wordsarray[wordpick].length; i++){
-                    if(wordchosen.charAt(i)==key) {
+                for (i=0; i<this.wordchosen.length; i++){
+                    if(this.wordchosen.charAt(i)==key) {
                         this.matchingPos.push(i);
                         hits=true;
                         $("."+i).removeClass("hideletter");
@@ -81,7 +161,7 @@ $(document).ready(function() {
                     this.notAmatch()
                 }
                 else if(!hits && this.numberofGuess==0){
-                    alert("Game over Bitch")
+                    alert("Game Over")
                 }
                 else if (hits){
                    this.correctguess.push(key)
@@ -102,30 +182,13 @@ $(document).ready(function() {
         
     }
 
-    var wordpick=Math.floor(Math.random()*words.wordsarray.length);
-    var wordchosen = words.wordsarray[wordpick];
+    words.selectword();
+    
     var audioElement = document.createElement("audio");
     audioElement.setAttribute("src", "assets/images/champions.mp3");
 
 
-    for (i=0; i<wordchosen.length; i++){
-        //create a new element div to have the char inside and assign a class of hideletter
-        var innerCharDivs= document.createElement("div");
-        innerCharDivs.setAttribute("class", i+ " hideletter innerposition"+ wordchosen.charAt(i));
-        innerCharDivs.innerHTML=wordchosen.charAt(i);
-        
-        /*create a new element div to have the previoius created div inside 
-        so i can hide the letter while keeping a bottom border visible*/
 
-        outerCharDivs=document.createElement("div");
-        outerCharDivs.setAttribute("class", "position-letter");
-        $(outerCharDivs).append(innerCharDivs);
-        //I will append this to the ID Created on the HTML
-        $("#word-to-guess").append(outerCharDivs);
-
-        words.pushWordChar(wordchosen.charAt(i))
-        console.log(words.charArray)
-    }
     document.onkeyup = function(event){  
         var keyhit=words.searchkey(event.key)  
         
